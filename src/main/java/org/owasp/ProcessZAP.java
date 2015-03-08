6d12d91d3ea933829601a1456f2fe2efc656c31c
unknown <java.development@gmail.com>
2015-03-08 16:40:23 +0100

    updated to Maven 3.2.1 Maven plugin plugin 3.4 ZAP Client version 2.4 semi-fixes for compile errors recreated tree structure from Maven plugin archetype

---
+++ src/main/java/org/owasp/ProcessZAP.java
@@ -0,0 +1,328 @@
+package org.owasp;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.zaproxy.clientapi.core.ApiResponse;
+import org.zaproxy.clientapi.core.ApiResponseElement;
+import org.zaproxy.clientapi.core.ClientApi;
+import org.zaproxy.clientapi.core.ClientApiException;
+
+/**
+ * Goal which touches a timestamp file.
+ */
+@Mojo( name = "process-zap", 
+       defaultPhase = LifecyclePhase.POST_INTEGRATION_TEST, 
+       threadSafe = true )
+public class ProcessZAP extends AbstractMojo {
+
+    private ClientApi zapClientAPI;
+    private Proxy proxy;
+
+    /**
+     * Location of the host of the ZAP proxy
+     */
+    @Parameter( defaultValue = "localhost", 
+    		    required = true)
+    private String zapProxyHost;
+
+    /**
+     * Location of the port of the ZAP proxy
+     */
+    @Parameter( defaultValue = "8080", 
+		        required = true)
+    private int zapProxyPort;
+
+    /**
+     * Location of the port of the ZAP proxy
+     */
+    @Parameter( required=true )
+    private String targetURL;
+
+    /**
+     * Switch to spider the URL
+     */
+    @Parameter( defaultValue="true" )
+    private boolean spiderURL;
+
+    /**
+     * Switch to scan the URL
+     */
+    @Parameter( defaultValue="true" )
+    private boolean scanURL;
+
+    /**
+     * Save session of scan
+     */
+    @Parameter( defaultValue="true" )
+    private boolean saveSession;
+
+    /**
+     * Switch to shutdown ZAP
+     */
+    @Parameter( defaultValue="true" )
+    private boolean shutdownZAP;
+
+    /**
+     * Save session of scan
+     */
+    @Parameter( defaultValue="tru"
+    		+ "e" )
+    private boolean reportAlerts;
+
+    /**
+     * Location to store the ZAP reports
+     */
+    @Parameter( defaultValue="${project.build.directory}/zap-reports" )
+    private String reportsDirectory;
+
+    /**
+     * Set the output format type, in addition to the XML report. Must be one of "none" or "json".
+     */
+    @Parameter ( defaultValue="none" )
+    private String format;
+
+    /**
+     * create a Timestamp
+     * 
+     * @return
+     */
+    private String dateTimeString() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        return sdf.format(cal.getTime());
+    }
+
+    /**
+     * create a temporary filename
+     * 
+     * @param prefix
+     *            if null, then default "temp"
+     * @param suffix
+     *            if null, then default ".tmp"
+     * @return
+     */
+    private String createTempFilename(String prefix, String suffix) {
+        StringBuilder sb = new StringBuilder("");
+        if (prefix != null)
+            sb.append(prefix);
+        else
+            sb.append("temp");
+
+        // append date time and random UUID 
+        sb.append(dateTimeString()).append("_").append(UUID.randomUUID().toString());
+
+        if (suffix != null)
+            sb.append(suffix);
+        else
+            sb.append(".tmp");
+
+        return sb.toString();
+    }
+
+    /**
+     * Change the ZAP API status response to an integer
+     *
+     * @param response the ZAP APIresponse code
+     * @return
+     */
+    private int statusToInt(ApiResponse response) {
+        return Integer.parseInt(((ApiResponseElement)response).getValue());
+    }
+
+    /**
+     * Search for all links and pages on the URL
+     *
+     * @param url the to investigate URL
+     * @throws ClientApiException
+     */
+    private void spiderURL(String url) throws ClientApiException {
+        zapClientAPI.spider.scan("apikey", url, "children");
+
+        while ( statusToInt(zapClientAPI.spider.status("scanid")) < 100) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                getLog().error(e.toString());
+            }
+        }
+    }
+
+    /**
+     * Scan all pages found at url
+     *
+     * @param url the url to scan
+     * @throws ClientApiException
+     */
+    private void scanURL(String url) throws ClientApiException {
+        zapClientAPI.ascan.scan("apikey", url, "true", "false", "scanpolicyname");
+
+        while ( statusToInt(zapClientAPI.ascan.status("")) < 100) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                getLog().error(e.toString());
+            }
+        }
+    }
+
+    /**
+     * Get all alerts from ZAP proxy
+     *
+     * @param json true for json form, false for xml format
+     * @return  all alerts from ZAProxy
+     * @throws Exception
+     */
+    private String getAllAlerts(String format) throws Exception {
+        URL url;
+        String result = "";
+
+        if (format.equalsIgnoreCase("xml") || 
+        	format.equalsIgnoreCase("html") ||
+        	format.equalsIgnoreCase("json")) {
+            url = new URL("http://zap/" + format + "/core/view/alerts");
+        } else {
+            url = new URL("http://zap/xml/core/view/alerts");
+        }
+
+        getLog().info("Open URL: " + url.toString());
+
+        HttpURLConnection uc = (HttpURLConnection) url.openConnection(proxy);
+        uc.connect();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                uc.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null) {
+            result = result + inputLine;
+        }
+
+        in.close();
+        return result;
+
+    }
+    
+    /**
+     * Get all alerts from ZAP proxy
+     *
+     * @param json true for json form, false for xml format
+     * @return  all alerts from ZAProxy
+     * @throws Exception
+     */
+    private String getAllAlertsFormat(String format) throws Exception {
+
+    	if (format.equalsIgnoreCase("xml") || 
+        	format.equalsIgnoreCase("html") ||
+        	format.equalsIgnoreCase("json")) {
+            return format;
+        } else {
+            return "xml";
+        }
+    	
+    }
+
+    /**
+     * execute the whole shabang
+     *
+     * @throws MojoExecutionException
+     */
+    public void execute() throws MojoExecutionException {
+    	System.setProperty("java.net.debug", "all");
+
+        try {
+
+            zapClientAPI = new ClientApi(zapProxyHost, zapProxyPort);
+            proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(zapProxyHost, zapProxyPort));
+
+            if (spiderURL) {
+                getLog().info("Spider the site [" + targetURL + "]");
+                spiderURL(targetURL);
+            } else {
+                getLog().info("skip spidering the site [" + targetURL + "]");
+            }
+
+            if (scanURL) {
+                getLog().info("Scan the site [" + targetURL + "]");
+                scanURL(targetURL);
+            } else {
+                getLog().info("skip scanning the site [" + targetURL + "]");
+            }
+
+            // filename to share between the session file and the report file
+            String fileName = "";
+            if (saveSession) {
+
+                fileName = createTempFilename("ZAP", "");
+
+                zapClientAPI.core.saveSession(fileName, "true","");
+            } else {
+                getLog().info("skip saveSession");
+            }
+
+            if (reportAlerts) {
+
+                // reuse fileName of the session file
+                if ((fileName == null) || (fileName.length() == 0))
+                    fileName = createTempFilename("ZAP", "");
+
+                String fileName_no_extension = FilenameUtils.concat(reportsDirectory, fileName);
+
+                try {
+                    String alerts = getAllAlerts(getAllAlertsFormat(format));
+                    String fullFileName = fileName_no_extension + "." + getAllAlertsFormat(format);
+                    FileUtils.writeStringToFile(new File(fullFileName), alerts);
+                    
+                    getLog().info("File save in format in ["+getAllAlertsFormat(format)+"]");
+                } catch (Exception e) {
+                    getLog().error(e.toString());
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            getLog().error(e.toString());
+            throw new MojoExecutionException("Processing with ZAP failed");
+        } finally {
+            if (shutdownZAP && (zapClientAPI != null)) {
+                try {
+                    getLog().info("Shutdown ZAProxy");
+                    zapClientAPI.core.shutdown("apikey");
+                } catch (Exception e) {
+                    getLog().error(e.toString());
+                    e.printStackTrace();
+                }
+            } else {
+                getLog().info("No shutdown of ZAP");
+            }
+        }
+    }
+
+}
---
+++ src/main/java/org/owasp/StartZAP.java
@@ -0,0 +1,132 @@
+package org.owasp;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.zaproxy.clientapi.core.ClientApi;
+
+
+/**
+ * Goal which will start ZAP proxy.
+ */
+@Mojo( name = "start-zap",
+       defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST,
+       threadSafe = true )
+public class StartZAP extends AbstractMojo
+{
+    /**
+     * Location of the ZAProxy program.
+     */
+	@Parameter( required=true )
+    private String zapProgram;
+    
+    /**
+     * Location of the host of the ZAP proxy
+     */
+	@Parameter( defaultValue="localhost", required=true )
+    private String zapProxyHost;
+
+    /**
+     * Location of the port of the ZAP proxy
+     */
+	@Parameter( defaultValue="8080", required=true )
+    private int zapProxyPort;
+
+    /**
+     * New session when you don't want to start ZAProxy.
+     */
+	@Parameter( defaultValue="false" )
+    private boolean newSession;
+
+    /**
+     * Sleep to wait to start ZAProxy
+     */
+	@Parameter( defaultValue="4000" )
+    private int zapSleep;
+    
+    public void execute()
+        throws MojoExecutionException
+    {
+    	try {
+            if (newSession) {
+                ClientApi zapClient = new ClientApi(zapProxyHost, zapProxyPort);
+                File tempFile = File.createTempFile("ZAP", null);
+                getLog().info("Create Session with temporary file [" + tempFile.getPath() + "]");
+                zapClient.core.newSession("apikey",tempFile.getPath(), "true");
+            } else {
+                File pf = new File(zapProgram);
+                Runtime runtime = java.lang.Runtime.getRuntime();
+                getLog().info("Start ZAProxy [" + zapProgram + "]");
+                getLog().info("Using working directory [" + pf.getParentFile().getPath() + "]");
+                final Process ps = runtime.exec(zapProgram, null, pf.getParentFile());
+                
+                // Consommation de la sortie standard de l'application externe dans un Thread separe
+                new Thread() {
+                    public void run() {
+                        try {
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(ps.getInputStream()));
+                            String line = "";
+                            try {
+                                while((line = reader.readLine()) != null) {
+                                    // Traitement du flux de sortie de l'application si besoin est
+                                    getLog().info(line);
+                                }
+                            } finally {
+                                reader.close();
+                            }
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+
+                // Consommation de la sortie d'erreur de l'application externe dans un Thread separe
+                new Thread() {
+                    public void run() {
+                        try {
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(ps.getErrorStream()));
+                            String line = "";
+                            try {
+                                while((line = reader.readLine()) != null) {
+                                    // Traitement du flux d'erreur de l'application si besoin est
+                                    getLog().info(line);
+                                }
+                            } finally {
+                                reader.close();
+                            }
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();       
+                
+            }
+            Thread.currentThread().sleep(zapSleep);
+        } catch(Exception e) {
+                e.printStackTrace();
+                throw new MojoExecutionException("Unable to start ZAP [" + zapProgram + "]");
+        }
+
+    }
+}
